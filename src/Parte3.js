1bb50c24910d3fe78a05bc1f3c88f532a0594098
Luan Marques <luanmarques1789@gmail.com>
2022-11-28 03:48:06 -0300

    dissolved table

--- src/Parte3.js
+++ src/Parte3.js
@@ -1,4 +1,4 @@
-import { H2 } from './components/Titles/Titles';
+import { H2, H3 } from './components/Titles/Titles';
 
 export default function Parte3() {
 	return (
@@ -10,63 +10,24 @@ export default function Parte3() {
 				qualquer outra tecnologia, ele tem os seus pontos fracos. Na tabela
 				abaixo, nota-se as suas vantagens e desvantagens.
 			</p>
-			<table className="table-auto">
-				<thead>
-					<tr>
-						<th>
-							<span>Vantagens</span>
-						</th>
-						<th>
-							<span>Desvantagens</span>
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>
-							<span>Desempenho aprimorado</span>
-						</td>
-						<td>
-							<span>Rodar Laravel em hospedagens comuns não é produtivo</span>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<span>Alta segurança</span>
-						</td>
-						<td>
-							<span>
-								Complexidade da estrutura de trabalho dificulta a instalação em
-								geral
-							</span>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<span>Migração de banco de dados</span>
-						</td>
-						<td>
-							<span>Custo alto para contratar desenvolvedores</span>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<span>Redução de custo com manutenção</span>
-						</td>
-						<td>
-							<span>Suporte limitado</span>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<span>“Baixa” curva de aprendizagem</span>
-						</td>
-						<td>
-							<span>Lento</span>
-						</td>
-					</tr>
-				</tbody>
-			</table>
+			<H3>Vantagens</H3>
+			<ul>
+				<li>Desempenho aprimorado</li>
+				<li>Rodar Laravel em hospedagens comuns não é produtivo</li>
+				<li>Alta segurança</li>
+				<li>
+					Complexidade da estrutura de trabalho dificulta a instalação em geral
+				</li>
+				<li>Migração de banco de dados</li>
+			</ul>
+			<H3>Desvantagens</H3>
+			<ul>
+				<li>Custo alto para contratar desenvolvedores</li>
+				<li>Redução de custo com manutenção</li>
+				<li>Suporte limitado</li>
+				<li>“Baixa” curva de aprendizagem</li>
+				<li>Lento</li>
+			</ul>
 		</div>
 	);
 }
